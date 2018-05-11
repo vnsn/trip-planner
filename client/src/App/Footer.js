871400bc225253563c777285979db7cd5c32b39b
wright0776 <matwright2010@gmail.com>
2018-05-11 10:29:12 -0600

    completed header, footer, and  login page (aside from auth) and added several components

---
+++ client/src/App/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className='footer'>
+            Copyright Jen, Ateeq & Matt | 2018 &copy;
+        </div>
+    )
+}
+
+export default Footer
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import {Provider} from 'react-redux';
+import store from './redux/index';
+
+
+import {BrowserRouter} from 'react-router-dom';
+
+import App from './App';
+import './style.css';
+
+ReactDOM.render(
+    <Provider store = {store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
+    document.getElementById('root')
+);
