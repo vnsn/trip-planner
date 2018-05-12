fd1c57de4cd883624e7322c53c50c8fa695a16bf
JE <28377077+vnsn@users.noreply.github.com>
2018-05-12 16:08:35 -0600

    added transportation-reducer and added that and reservations reducer to combinereducers

--- client/src/redux/index.js
+++ client/src/redux/index.js
@@ -3,8 +3,10 @@ import thunk from "redux-thunk";
 
 import trips from "./trips-reducer";
 import destinations from "./destinations-reducer";
+import reservations from "./reservations-reducer";
+import transportation from "./transportation-reducer";
 
-const store = createStore(combineReducers({trips, destinations}), 
+const store = createStore(combineReducers({trips, destinations, reservations, transportation}), 
 window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 applyMiddleware(thunk));
 
